19487ae709f7b0f33c6878a5c7ad8c8c6ea09e77
Toshimasa Takahashi <kanayagawa@gmail.com>
2019-02-21 07:59:15 +0900

    4.1.1 パイプの基本

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: ` 
-    <form>
-      <label for="zip">郵便番号：</label>
-      <input id="zip" name="zip" type="text" size="10" 
-        (keypress)="mask($event)"/> 
-    </form>
-    `
-})
-export class AppComponent  {
-  mask(e: any) {
-    let k = e.which;
-    if (!((k >= 48 && k <= 57) || k === 45 || k === 8 || k === 0)) {
-      e.preventDefault();
-    }
-  }
+  template: `<pre>{{obj | json}}</pre>`
 }
+  )
+
+export class AppComponent {
+  obj: any = {
+    name: 'トクジロウ',
+    gender: undefined,
+    birth: new Date(2007, 7, 15),
+    age: 3,
+    family: ['リンリン', 'サチ', 'ニンザブロウ'],
+    work: function () { /* メソッドの中身 */},
+    other: {
+      favorite: 'ひまわり',
+      memo: '偏屈爺さん'
+    }
+  };
+}
+
+
