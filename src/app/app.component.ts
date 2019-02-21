ce9cf86f55ec35c897cd7fad66b0e4bf6ce4a54a
Toshimasa Takahashi <kanayagawa@gmail.com>
2019-02-21 23:41:15 +0900

    4.2.1 ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,23 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<pre>{{obj | json}}</pre>`
+    template: `
+      <form>
+        <label for="show">表示/非表示：</label>
+        <input id="show" name="show" type="checkbox" 
+          [(ngModel)]="show"/>
+      </form>
+      <div *ngIf="show">
+        <p>　21日21時22分頃、北海道で最大震度6弱の地震がありました。
+
+          　震源は胆振地方中東部、震源の深さはおよそ30km、地震の規模を示すマグニチュードは5.7と推定されます。
+
+          　この地震で厚真町では最大震度6弱を、安平町、むかわ町で震度5強、札幌市や千歳市で震度5弱を観測しました。</p>
+      </div>
+    `
 }
   )
 
 export class AppComponent {
-  obj: any = {
-    name: 'トクジロウ',
-    gender: undefined,
-    birth: new Date(2007, 7, 15),
-    age: 3,
-    family: ['リンリン', 'サチ', 'ニンザブロウ'],
-    work: function () { /* メソッドの中身 */},
-    other: {
-      favorite: 'ひまわり',
-      memo: '偏屈爺さん'
-    }
-  };
+  show = false;
 }
 
 
