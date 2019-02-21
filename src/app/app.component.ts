50c340946cdcde7986ef2755c9deb2943dba9193
Toshimasa Takahashi <kanayagawa@gmail.com>
2019-02-21 23:49:03 +0900

    4.2.1 スタイルバインディングで表示/非表示を変更

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,10 @@ import { Component } from '@angular/core';
     template: `
       <form>
         <label for="show">表示/非表示：</label>
-        <input id="show" name="show" type="checkbox" 
-          [(ngModel)]="show"/>
+        <input id="show" name="show" type="checkbox"
+               [(ngModel)]="show"/>
       </form>
-      <div *ngIf="show">
+      <div [style.display]="show? 'inline': 'none'">
         <p>　21日21時22分頃、北海道で最大震度6弱の地震がありました。
 
           　震源は胆振地方中東部、震源の深さはおよそ30km、地震の規模を示すマグニチュードは5.7と推定されます。
